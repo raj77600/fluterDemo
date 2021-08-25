a5052151b4fc30dd90ecb769a2d61c17a65c783b
GCELL\rajinder <rajinder@graycelltech.com>
2021-08-25 15:00:48 +0530

    Day 15 Gried View Complate

--- lib/pages/_homepage.dart
+++ lib/pages/_homepage.dart
@@ -21,43 +21,72 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     loadData();
   }
-
   loadData() async {
-    await Future.delayed(Duration(seconds: 2));
-    final catalogJson =
-    await rootBundle.loadString("assets/files/catalog.json");
-    final decodedData = jsonDecode(catalogJson);
-    var productsData = decodedData["products"];
-    CatalogModel.items = List.from(productsData)
-        .map<Item>((item) => Item.fromMap(item))
-        .toList();
-    setState(() {});
+      await Future.delayed(Duration(seconds: 2));
+      final catalogJson =
+      await rootBundle.loadString("assets/files/catalog.json");
+      final decodedData = jsonDecode(catalogJson);
+      var productsData = decodedData["products"];
+      CatalogModel.items = List.from(productsData)
+          .map<Item>((item) => Item.fromMap(item))
+          .toList();
+      setState(() {});
+    }
 
-
-
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) => itemwidget(
-            item: CatalogModel.items[index],
-          ),
-        )
-            : Center(
-          child: CircularProgressIndicator(),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Catalog App"),
         ),
-      ),
-      drawer: mydraers(),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+              ? GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+            ),
+            itemBuilder: (context, index) {
+              final item = CatalogModel.items![index];
+              return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: GridTile(
+                    header: Container(
+                      child: Text(
+                        item.name,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    child: Image.network(
+                      item.image,
+                    ),
+                    footer: Container(
+                      child: Text(
+                        item.price.toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ));
+            },
+            itemCount: CatalogModel.items!.length,
+          )
+              : Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        drawer: mydraers(),
+      );
+    }
   }
-}
